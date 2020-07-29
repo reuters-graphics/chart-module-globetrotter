93bfe465c5b80e885685c15d683c733e31c048bd
gurmanbh <gurmanbh@gmail.com>
2020-07-29 11:15:05 +0800

    build

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b5d59fd813683b76c282";
+/******/ 	var hotCurrentHash = "1fbc3393e5e1b228b5ed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
